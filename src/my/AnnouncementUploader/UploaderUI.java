d40d9da75f5e13a00bbdb192df29c47a3918abfe
Aaron-G-96 <aamigo89@gmail.com>
2015-06-08 09:04:07 -0400

    Added option to append or re-write

--- src/my/AnnouncementUploader/UploaderUI.java
+++ src/my/AnnouncementUploader/UploaderUI.java
@@ -43,6 +43,7 @@ public class UploaderUI extends javax.swing.JFrame {
         cmb_month = new javax.swing.JComboBox();
         cmb_year = new javax.swing.JComboBox();
         cmb_day = new javax.swing.JComboBox();
+        opt_append = new javax.swing.JRadioButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -54,7 +55,7 @@ public class UploaderUI extends javax.swing.JFrame {
                 btn_generateActionPerformed(evt);
             }
         });
-        getContentPane().add(btn_generate, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 700, 150, 40));
+        getContentPane().add(btn_generate, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 680, 150, 40));
 
         btn_add.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         btn_add.setText("Add Announcement");
@@ -63,7 +64,7 @@ public class UploaderUI extends javax.swing.JFrame {
                 btn_addActionPerformed(evt);
             }
         });
-        getContentPane().add(btn_add, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 600, 210, 80));
+        getContentPane().add(btn_add, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 580, 210, 80));
 
         txt_announcement.setColumns(20);
         txt_announcement.setLineWrap(true);
@@ -75,25 +76,30 @@ public class UploaderUI extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(txt_announcement);
 
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, 590, 380));
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 180, 590, 380));
 
         lbl_announceNum.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         lbl_announceNum.setText("Enter Announcement 1:");
-        getContentPane().add(lbl_announceNum, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 150, -1, -1));
+        getContentPane().add(lbl_announceNum, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 130, -1, -1));
 
-        cmb_month.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novemember", "December" }));
-        getContentPane().add(cmb_month, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 70, 140, -1));
+        cmb_month.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" }));
+        cmb_month.setToolTipText("");
+        getContentPane().add(cmb_month, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 70, 140, -1));
 
         cmb_year.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "2015", "2016", "2017", "2018", "2019", "2020" }));
-        getContentPane().add(cmb_year, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 70, 110, -1));
+        getContentPane().add(cmb_year, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 70, 110, -1));
 
-        cmb_day.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "1", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" }));
+        cmb_day.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" }));
         cmb_day.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 cmb_dayActionPerformed(evt);
             }
         });
-        getContentPane().add(cmb_day, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 70, 110, -1));
+        getContentPane().add(cmb_day, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 70, 110, -1));
+
+        opt_append.setSelected(true);
+        opt_append.setText("Add to Existing");
+        getContentPane().add(opt_append, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 750, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -105,7 +111,13 @@ public class UploaderUI extends javax.swing.JFrame {
             String text = "This is a text file";
             String userHomeFolder = System.getProperty("user.home");
             File file = new File(userHomeFolder + "/Desktop", "Announcements.xml");
-            writer = new BufferedWriter(new java.io.FileWriter(file, true));
+            
+            if(opt_append.isSelected()){
+                writer = new BufferedWriter(new java.io.FileWriter(file, true));
+            }else{
+                writer = new BufferedWriter(new java.io.FileWriter(file, false));
+            }
+            
             writer.write("\n<date>" + getDate() + "</date>");
             for (int i = 0; i < announceList.size(); i++){
                 writer.write("\n<announcement>" + announceList.get(i) + "</announcement>");
@@ -193,6 +205,7 @@ public class UploaderUI extends javax.swing.JFrame {
     private javax.swing.JComboBox cmb_year;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lbl_announceNum;
+    private javax.swing.JRadioButton opt_append;
     private javax.swing.JTextArea txt_announcement;
     // End of variables declaration//GEN-END:variables
 }
